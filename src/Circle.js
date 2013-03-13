3542386afe653daf6607d58265489f4a24ad918a
colinrdavidson <colinrdavidson@gmail.com>
2013-03-13 02:59:43 -0400

    New Point and Circle GC classes.

---
+++ src/Circle.js
@@ -0,0 +1,124 @@
+exports = Class(function () {
+
+  this.init = function (layer, foc, loc){
+    //foc is for focus ie centre
+    //loc is for loci ie point on circle
+  
+    this.layer = layer;
+    this.foc = foc;
+    this.loc = loc;
+    this.radius = foc.distanceToPoint(loc);
+  }
+  
+  this.isSame = function (Cir) {
+    if ((Cir instanceof Circle) && this.foc.isSame(Cir.foc) && this.radius == Cir.radius){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  
+  this.copy = function (layer) {
+    return new Circle(layer, this.foc, this.loc);
+  }
+  
+  this.intersectsWith = function (shape) {
+    var pointsOfIntersection = [];
+  
+    if (shape instanceof Circle){
+      var x0 = this.foc.x;
+      var y0 = this.foc.y;
+      var r0 = pointPointDistance(this.foc.x, this.foc.y, this.loc.x, this.loc.y);
+      var x1 = shape.foc.x; 
+      var y1 = shape.foc.y;
+      var r1 = pointPointDistance(shape.foc.x, shape.foc.y, shape.loc.x, shape.loc.y);
+      var a, dx, dy, d, h, rx, ry;
+      var x2, y2;
+  
+      /* dx and dy are the vertical and horizontal distances between
+       * the circle centers.
+       */
+      dx = x1 - x0;
+      dy = y1 - y0;
+  
+      /* Determine the straight-line distance between the centers. */
+      d = Math.sqrt((dy*dy) + (dx*dx));
+  
+      /* Check for solvability. */
+      if (d > (r0 + r1)) {
+        /* no solution. circles do not intersect. */
+        console.log("No intersection, beside each other");
+      }
+      else if (d < Math.abs(r0 - r1)) {
+        /* no solution. one circle is contained in the other */
+        console.log("No intersection, one inside other");
+      }
+      else {
+  
+        /* 'point 2' is the point where the line through the circle
+         * intersection points crosses the line between the circle
+         * centers.  
+         */
+  
+        /* Determine the distance from point 0 to point 2. */
+        a = ((r0*r0) - (r1*r1) + (d*d)) / (2.0 * d) ;
+  
+        /* Determine the coordinates of point 2. */
+        x2 = x0 + (dx * a/d);
+        y2 = y0 + (dy * a/d);
+  
+        /* Determine the distance from point 2 to either of the
+         * intersection points.
+         */
+        h = Math.sqrt((r0*r0) - (a*a));
+  
+        /* Now determine the offsets of the intersection points from
+         * point 2.
+         */
+        rx = -dy * (h/d);
+        ry = dx * (h/d);
+  
+        /* Determine the absolute intersection points. */
+        var xi = x2 + rx;
+        var xi_prime = x2 - rx;
+        var yi = y2 + ry;
+        var yi_prime = y2 - ry;
+  
+        var point1 = new Point(this.layer, xi, yi);
+        var point2 = new Point(this.layer, xi_prime, yi_prime);
+  
+        if (point1.isSame(point2)){
+          pointsOfIntersection.push(point1);
+        }
+        else {
+          pointsOfIntersection.push(point1);
+          pointsOfIntersection.push(point2);
+        }
+      }
+    }
+  
+    else if (shape instanceof Line){
+      return shape.intersectsWith(this);
+    }
+    else{
+      console.log("Not a shape");
+    }
+  
+    return pointsOfIntersection;
+  }
+  
+  this.toString = function() {
+    return "Circle: (" + this.foc.toString() + ", " + this.loc.toString() + ")";
+  }
+  
+  this.isInArray = function (array){
+    //assume this array is full of points
+    for(var i = 0; i < array.length; i++){
+      if (this.isSame(array[i]))
+        return true;
+    }
+  
+    return false;
+  }
+});
---
+++ src/Point.js
@@ -0,0 +1,111 @@
+exports = Class(function() {
+
+  this.init = function(layer, x, y){
+    this.layer = layer;
+    this.x = Math.round(x*10000000)/10000000;
+    this.y = Math.round(y*10000000)/10000000;
+  }
+  
+  this.copy = function (layer) {
+    if (!layer){
+      return new Point(this.layer, this.x, this.y);
+    }
+    else
+      return new Point(layer, this.x, this.y);
+  }
+  
+  this.isSame = function (pt) {
+    if ((pt instanceof Point) && (this.x == pt.x && this.y == pt.y)){
+      return true;
+    }
+    else{
+      return false; 
+    }
+  }
+  
+  this.toString = function (){
+    return "Point: (" + this.x + ", " + this.y + ")";
+  }
+  
+  this.isInArray = function (array){
+    //assume this array is full of points
+    for(var i = 0; i < array.length; i++){
+      if (this.isSame(array[i])){
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  this.distanceToPoint = function (point) {
+    return pointPointDistance(this.x, this.y, point.x, point.y);
+  }
+  
+  this.distanceToLine = function (line) {
+    if (line instanceof Line){
+      if (this.projectedOnto(line)){
+        return this.distanceToPoint(this.projectedOnto(line));
+      }
+      else{
+        return Math.min(this.distanceToPoint(line.pt1), this.distanceToPoint(line.pt2));
+      }
+    }
+  }
+  
+  this.distanceToCircle = function (circle) {
+    var d = pointPointDistance(this.x, this.y, circle.foc.x, circle.foc.y);
+    var r = circle.radius;
+  
+    return Math.abs(d - r);
+  }
+  
+  this.distanceTo = function (object) {
+    if (object instanceof Point){
+      return this.distanceToPoint(object);
+    }
+    else if (object instanceof Line){
+      return this.distanceToLine(object);
+    }
+    else if (object instanceof Circle){
+      return this.distanceToCircle(object);
+    }
+    else{
+      console.log("Trying to check distance to not an object");
+    }
+  }
+  
+  this.projectedOnto = function (line) {
+    //P1 is first point of line, P2 is second point of line, P3 is point to project
+    var x1 = line.pt1.x;
+    var y1 = line.pt1.y;
+    var x2 = line.pt2.x;
+    var y2 = line.pt2.y;
+    var x3 = this.x;
+    var y3 = this.y;
+  
+    //(dx2, dy2) is vector from P1 to P2, (dx3, dy3) is vector from P1 to P3
+    var dx2 = x2 - x1;
+    var dy2 = y2 - y1;
+    var dx3 = x3 - x1;
+    var dy3 = y3 - y1;
+  
+    //P4 is P3 projected onto the line
+    //projScale is the scalar for the projection
+    var projScale = (dx2*dx3 + dy2*dy3)/(dx2*dx2 + dy2*dy2);
+  
+    //(dx4, dy4) is the vector from P1 to P4
+    var dx4 = projScale*dx2;
+    var dy4 = projScale*dy2;
+  
+    //P4 is the projected point
+    x4 = x1 + dx4;
+    y4 = y1 + dy4;
+    
+    var dummyPoint = new Point(this.layer, x4, y4);
+  
+    //point in segment
+    if (line.containsPoint(dummyPoint)){
+      return dummyPoint;
+    }
+  }
+});
