12e50fb14fa3e8ec1cd47547ece462b1e8a81259
Bill <billvaleriote@gmail.com>
2013-03-18 01:18:09 -0700

    Showing potential points on hover, this doesn't really do anything unless youre using a browser

--- src/view/PotentialPointView.js
+++ src/view/PotentialPointView.js
@@ -1,18 +1,24 @@
 import ui.ImageView as ImageView;
+import ui.resource.Image as Image;
 import src.view.PointView as PointView;
 
 exports = Class(PointView, function (supr) {
   this.init = function (opts) {
+
+    var newOpts = { image: "resources/images/circle-blank.png" };
+
+    opts = merge(newOpts, opts);
+
     supr(this, 'init', [opts]);
 
-//    this doesn't work so we should use just a transparent image or something?
-//    for now I'm going to use just a blue one 
-//    this.style.visible = false;
+    this.on("InputOver", bind(this, function (over, overCount, atTarget) {
+      var image = new Image({url: "resources/images/circle-red.png"});
+      this.setImage(image);
+    }));
 
-      
-
-    this.on("InputOver", function (event) {
-      this.style.visible = true;
-    });
+    this.on("InputOut", bind(this, function (over, overCount, atTarget) {
+      var image = new Image({url: "resources/images/circle-blank.png"});
+      this.setImage(image);
+    }));
   };
 });
