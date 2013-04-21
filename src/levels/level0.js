17380e5448c50c6b201eaaaf1642859dd0328ccc
colinrdavidson <colinrdavidson@gmail.com>
2013-04-21 17:49:24 -0400

    Tracking levels folder now.

---
+++ src/levels/level0.js
@@ -0,0 +1,32 @@
+var level0 = {
+  name: "level0",
+  state:
+  {
+    points:
+    {
+      0: { x: 100, y: 200 },
+      1: { x: 300, y: 200 },
+      2: { x: 200, y: 150 },
+    },
+    lines:
+    {
+    },
+    circles:
+    {
+    }
+  },
+  goalState:
+  {
+    points:
+    {
+      0: { x: 0, y: 0 },
+    },
+    lines:
+    {
+    },
+    circles:
+    {
+    }
+  }
+}
+Levels[level0.name] = level0;
