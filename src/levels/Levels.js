96293eeeaf0520a683af9e13b5a8e4be8efc8f85
Bill <billvaleriote@gmail.com>
2013-04-21 23:51:34 -0400

    Added the existing levels to the Levels container object.

--- src/levels/Levels.js
+++ src/levels/Levels.js
@@ -1,3 +1,18 @@
 import src.levels.intro0 as intro0;
+import src.levels.intro1 as intro1;
+import src.levels.intro2 as intro2;
+import src.levels.level0 as level0;
+//what happened to level 1?!
+import src.levels.level2 as level2;
+import src.levels.prop1 as prop1;
+import src.levels.prop2 as prop2;
+
 
 exports.intro0 = intro0;
+exports.intro1 = intro1;
+exports.intro2 = intro2;
+exports.level0 = level0;
+//what happened to level 1?!
+exports.level2 = level2;
+exports.prop1 = prop1;
+exports.prop2 = prop2;
