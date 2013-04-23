9367f30d85020bc6be7a6cf65d7e04a685f65661
Bill <billvaleriote@gmail.com>
2013-04-23 16:18:08 -0400

    Moved most of the stuff out of gamescreen into the game controller.

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -6,16 +6,23 @@ import src.model.Circle as Circle;
 import src.model.State as State;
 import src.ModelViewHash as Hash;
 import src.levels.Levels as Levels;
+import src.view.PointView as PointView;
+import src.view.PotentialPointView as PotentialPointView;
+import src.view.LineView as LineView;
+import src.view.CircleView as CircleView;
+import src.view.GoalPointView as GoalPointView;
+import src.view.GoalLineView as GoalLineView;
 
 exports = Class(function () {
 
-  this.init = function (drawer) {
-    this.drawer = drawer;
+  this.init = function (gameScreen) {
+    this.gameScreen = gameScreen;
     this.state = new State();
     this.goalState = new State();
     this.levelName;
     this.history = [];
     this.hash = new Hash();
+    this.selectedShapes = [];
   }
 
 //Level Handling  
@@ -76,7 +83,7 @@ exports = Class(function () {
 //Add Methods
   this.addGoalState = function (levelState) {
     this.goalState.add(levelState);
-    this.drawer.drawGoals(this.goalState);
+    this.drawGoals(this.goalState);
   };
 
   this.add = function (object) {
@@ -85,26 +92,132 @@ exports = Class(function () {
   }
 
 //Drawing methods 
-  this.draw = function (object, colour) {
-    if (!object){
-      this.drawer.draw(game.state, colour);
-    }
-    else{
-      this.drawer.draw(object, colour);
-    }
-  }
-  
-  this.clearLayer = function (layer) {
-    if (!layer){
-      this.drawer.clearLayer();
-    }
-    else if (Object.prototype.toString.call(layer) === '[object Array]'){
-      for (var i = 0; i < layer.length; i++){
-        this.clearLayer(layer[i]);
+  this.draw = function (object) {
+    if (Object.prototype.toString.call(object) === '[object Array]'){
+      for (var i = 0; i < object.length; i++){
+        this.draw(object[i]);
       }
     }
-    else{
-      this.drawer.clearLayer(layer);
+    else {
+      switch (object.type) {
+        case "State":
+          this.draw(object.pointsLinesCircles());
+          break;
+           
+        case "Point":
+          if (object.potential) {
+            var potentialPointView = new PotentialPointView({
+              point: object,
+              superview: this.gameScreen
+            });
+            potentialPointView.on('PotentialPointView:click', bind(this, this.potentialPointSelect));
+            game.hash.add({model: object, view: potentialPointView});
+          }
+          else {
+            var pointView = new PointView({
+              point: object,
+              superview: this.gameScreen
+            });
+            pointView.on('PointView:click', bind(this, this.pointSelect));
+            game.hash.add({model: object, view: pointView});
+          }
+          break;
+
+        case "Line":
+          var lineView = new LineView({
+            line: object,
+            superview: this.gameScreen
+          });
+          lineView.on('LineView:click', bind(this, this.lineSelect));
+          game.hash.add({model: object, view: lineView});
+          break;
+
+        case "Circle":
+          var circleView = new CircleView({
+            circle: object,
+            superview: this.gameScreen
+          });
+          circleView.on('CircleView:click', bind(this, this.circleSelect));
+          game.hash.add({model: object, view: circleView});
+          break;
+      }
     }
   }
+
+  this.drawGoals = function (object) {
+    if (Object.prototype.toString.call(object) === '[object Array]'){
+      for (var i = 0; i < object.length; i++){
+        this.drawGoals(object[i]);
+      }
+    }
+    else {
+      switch (object.type) {
+        case "State":
+          this.drawGoals(object.pointsLinesCircles());
+          break;
+           
+        case "Point":
+          var pointView = new GoalPointView({
+            point: object,
+            superview: this.gameScreen
+          });
+          break;
+
+        case "Line":
+          var lineView = new GoalLineView({
+            line: object,
+            superview: this.gameScreen
+          });
+          break;
+
+        case "Circle":
+          var circleView = new GoalCircleView({
+            circle: object,
+            superview: this.gamescreen
+          });
+          break;
+      }
+    }
+  }
+  this.pointSelect = function (pointView) {
+    var point = pointView._point;
+
+    this.selectShape(point);
+  };
+
+  this.potentialPointSelect = function (potentialPointView) {
+    var point = potentialPointView._point;
+
+    game.promotePotentialPoint(point);
+    potentialPointView.removeFromSuperview();
+
+    var newPointView = game.hash.byModel[point];
+    newPointView.select();
+    this.selectShape(point);
+  };
+
+  this.selectShape = function (shape) {
+    if (this.selectedShapes.length == 0) {
+      this.selectedShapes.push(shape);
+			game.hash.byModel[shape].select();
+    }
+    else if (this.selectedShapes.length == 1) {
+      if (this.selectedShapes[0] != shape) {
+        this.selectedShapes.push(shape);
+				game.hash.byModel[shape].select();
+      }
+    }
+    else {
+      if (this.selectedShapes[1] != shape) {
+        var unselected = this._selectedShapes.shift();
+        var unselectedView = game.hash.byModel[unselected];
+        unselectedView.unselect();
+
+        this.selectedShapes.push(shape);
+				game.hash.byModel[shape].select();
+      }
+    }
+
+    console.log("Point 1: " + this.selectedShapes[0] + " Point 2: " + this.selectedShapes[1]);
+  };
 });
